84f2465f4d200540251c42441e7eff4d3dc6648d
Cole miller <colemiller2002@gmail.com>
2025-03-06 21:25:06 -0500

    Updated main.py, added init for gui

--- main.py
+++ main.py
@@ -1,20 +1,10 @@
 from game.board import Board
-from visualization.board_graph import visualize_board
+from gui.pygame_gui import CatanGame
 
 def main():
     board = Board()
-    print("Generated Unique Catan Game:")
-    print("\nHexes:")
-    for hex_obj in board.hexes.values():
-        print(hex_obj)
-    print("\nSpots:")
-    for spot in board.spots.values():
-        print(spot)
-    print("\nRoads:")
-    for road in board.roads.values():
-        print(road)
+    gui = CatanGame(board)
+    gui.run()
     
-    visualize_board(board)
-
 if __name__ == "__main__":
     main()
